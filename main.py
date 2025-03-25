e46b546650ee1b29f4f4dbec500d24dfc35b4493
Bayos-Eurielle <eurielle0052@gmail.com>
2025-03-25 19:56:14 +0800

    add eurielle's part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # HighPYve - Exercise 7
+product_listing = []
 def order_details():
     # TODO (Ocariza): Display a welcome message and 
     # continuously ask the user to add products if they want to
@@ -7,7 +8,14 @@ def order_details():
 def add_product():
     # TODO (Bayos): Enter product details: name, price, and quantity
     # Append the entered details as a list
-    pass
+    product_name = input("Product Name: ")
+    price = int(input("Price: "))
+    quantity = int(input("Quantity: "))
+    total = price * quantity
+    product_listing.append([product_name, price, quantity, total])
+
+#     print (product_listing)
+# add_product()
 
 def customer_details():
     # TODO (Anipan): Collect customer details, including optional senior ID
@@ -20,3 +28,4 @@ def calculate_total(senior_id_number):
 def display_summary(customer_name, senior_id_number):
     # TODO (Tolentino): Compute the grand total and display the order details
     pass
+
