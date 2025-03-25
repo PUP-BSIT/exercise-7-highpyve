d75763ab7f186cf86cce339b4a4b7b4cb3802f0a
Ocariza_JairaIsabel <jairaocariza@gmail.com>
2025-03-25 19:38:02 +0800

    Upload TODO tasks

---
+++ main.py
@@ -0,0 +1,22 @@
+# HighPYve - Exercise 7
+def order_details():
+    # TODO (Ocariza): Display a welcome message and 
+    # continuously ask the user to add products if they want to
+    pass
+
+def add_product():
+    # TODO (Bayos): Enter product details: name, price, and quantity
+    # Append the entered details as a list
+    pass
+
+def customer_details():
+    # TODO (Anipan): Collect customer details, including optional senior ID
+    pass
+
+def calculate_total(senior_id_number):
+    # TODO (Bartolome): Apply 10% discount if the customer has a senior ID
+    pass
+
+def display_summary(customer_name, senior_id_number):
+    # TODO (Tolentino): Compute the grand total and display the order details
+    pass
