f882fef6a4ee4b815b7217f64e1110dc74dfc1b9
Anipan_SigmundKristoffer <krisanipan2205@gmail.com>
2025-03-25 20:26:43 +0800

    Add Kristoffer's Part

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # HighPYve - Exercise 7
 product_listing = []
+
 def order_details():
+    # TODO (Ocariza): Display a welcome message and 
+    # continuously ask the user to add products if they want to
     print("Welcome to HighPyve Order System")
     while True:
         add_product()
@@ -19,8 +22,13 @@ def add_product():
     product_listing.append([product_name, price, quantity, total])
 
 def customer_details():
-    # TODO (Anipan): Collect customer details, including optional senior ID
-    pass
+    # TODO (Anipan): Collect customer details, including optional senior ID.
+    # This will include capturing customer's name and checking for 
+    # discount eligibility.
+    customer_name = input("Enter customer name: ")
+    senior_id_number = input("Enter senior ID number (If applicable): ")
+    calculate_total(senior_id_number)
+    display_summary(customer_name, senior_id_number)
 
 def calculate_total(senior_id_number):
     # TODO (Bartolome): Apply 10% discount if the customer has a senior ID
