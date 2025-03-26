fb71e3b68312d541f0be7c8ce50d62ed5bbb4af3
Tolentino-MaRose <maroseltolentino@iskolarngbayan.pup.edu.ph>
2025-03-26 21:38:35 +0800

    check coding guidelines

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
 # HighPYve - Exercise 7
 product_listing = []
 
-def order_details():
+def get_order_details():
     # TODO (Ocariza): Display a welcome message and 
     # continuously ask the user to add products if they want to
-    print("Welcome to HighPyve Order System")
+    
+    print("\nWelcome to HighPyve Order System")
+    
     while True:
-        add_product()
-        decision = input("Do you want to add another item? (y/n): ").lower()
+        append_product()
+        decision = input("\nDo you want to add another item? (y/n): ").lower()
+        
         if decision != 'y': 
-            customer_details()
+            get_customer_details()
             break
 
-def add_product():
+def append_product():
     # TODO (Bayos): Enter product details: name, price, and quantity
     # Append the entered details as a list
-    product_name = input("Product Name: ")
-    price = float(input("Price: "))
-    quantity = int(input("Quantity: "))
+    
+    product_name = input("\nEnter Product Name: ")
+    price = float(input("Enter Price: "))
+    quantity = int(input("Enter Quantity: "))
     total = price * quantity
+    
     product_listing.append([product_name, price, quantity, total])
 
-def customer_details():
+def get_customer_details():
     # TODO (Anipan): Collect customer details, including optional senior ID.
     # This will include capturing customer's name and checking for 
     # discount eligibility.
-    customer_name = input("Enter customer name: ")
-    senior_id_number = input("Enter senior ID number (If applicable): ")
-    calculate_total(senior_id_number)
-    display_order_summary(customer_name, senior_id_number)
+    
+    customer_name = input("\nEnter customer name: ")
+    senior_id = input("Enter senior ID number (If applicable): ")
+    get_total_amount(senior_id)
+    display_order_summary(customer_name, senior_id)
 
-def calculate_total(senior_id_number):
+def get_total_amount(senior_id):
     # TODO (Bartolome): Apply 10% discount if the customer has a senior ID
+    # Compute grand total
+    
     global grand_total
     grand_total = 0
-    if senior_id_number:
+    
+    if senior_id:
         for product in product_listing:
                 product[3] = product[3] * 0.9
 
     for product in product_listing:
         grand_total += product[3]
 
-def display_order_summary(customer_name, senior_id_number):
+def display_order_summary(customer_name, senior_id):
     # TODO (Tolentino): Compute the grand total and display the order details
     
     print("\n-------------------Order Summary-------------------")
@@ -50,14 +59,14 @@ def display_order_summary(customer_name, senior_id_number):
 
     for product in product_listing:
         print(f"{product[0]}\t\t {product[1]}\t       "
-              f"{product[2]}\t {product[3]}")
+              f"{product[2]}\t {product[3]:.2f}")
         
     print("---------------------------------------------------")       
     print(f"\nCustomer Name: {customer_name}")
     
-    if senior_id_number:
-        print("Senior ID Number: ")
+    if senior_id:
+        print(f"Senior ID Number: {senior_id}")
         
     print(f"Grand Total: {grand_total:.2f}")
 
-order_details()
+get_order_details()
